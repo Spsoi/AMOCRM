7d280935c78dd11c1f586cc98277900b23cfde89
Spsoi <42588004+Spsoi@users.noreply.github.com>
2021-07-25 11:43:52 +0300

    Create Editing Patch Lead.js

---
+++ Lead/Editing Patch Lead.js
@@ -0,0 +1,34 @@
+//https://www.amocrm.com/developers/content/crm_platform/leads-api/#leads-edit
+let data = {
+    0: {
+        "id": 24528107, // id lead сделки
+        "custom_fields_values": [
+            {
+                "field_id": 615173, // id поля
+                "values": [
+                    {
+                        "value": '4218351328071037443'
+                    }
+                ]
+            },
+     
+            {
+                "field_id": 615149,// id поля
+                "values": [
+                    {
+                        "value": 'https://www.tiktok.com/'
+                    }
+                ]
+            },
+        ]
+    },
+    
+}
+
+$.ajax({
+   type: 'PATCH',
+   url: '/api/v4/leads',
+   data: JSON.stringify(data),
+   processData: false,
+   contentType: 'application/merge-patch+json',
+});
