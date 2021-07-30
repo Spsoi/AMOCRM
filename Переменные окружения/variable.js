0e9be75d72897489962508cfd2019b32308500c5
Spsoi <42588004+Spsoi@users.noreply.github.com>
2021-07-30 11:29:20 +0300

    Create variable.js

---
+++ Переменные окружения/variable.js
@@ -0,0 +1,12 @@
+//https://www.amocrm.ru/developers/content/web_sdk/env_variables
+// получение карточек сущностей прикреплённых к сущности
+AMOCRM.data.current_card.linked_forms.form_models.models; 
+
+// получение поля карточки
+AMOCRM.data.current_card.model.attributes["CFV[187495]"]
+
+//получение id сущности
+AMOCRM.data.current_card.id;
+
+// имя сущности
+AMOCRM.data.current_card.$name.text();
