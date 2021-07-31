6e1ef0e77e465ac622facb4f7181a4fd85681d76
Spsoi <42588004+Spsoi@users.noreply.github.com>
2021-07-31 20:37:30 +0300

    Update variable.js

--- Переменные окружения/variable.js
+++ Переменные окружения/variable.js
@@ -2,6 +2,21 @@
 // получение карточек сущностей прикреплённых к сущности
 AMOCRM.data.current_card.linked_forms.form_models.models; 
 
+//Забираем из Лида ИД компании
+let company_id = AMOCRM.data.current_card.id;
+if (AMOCRM.data.current_entity == 'companies') {
+    company_id = AMOCRM.data.current_card.id;
+}
+if (AMOCRM.data.current_entity == 'leads') {
+    let carts = AMOCRM.data.current_card.linked_forms.form_models.models;
+    for (let i = 0; i < carts.length; i++) {
+        if (carts[i].url == "/ajax/companies/detail/") { // забираем ИД компании у сделки
+            id = carts[i].attributes["ID"];
+            company_id  = id;
+        }
+    }
+}
+
 // получение поля карточки
 AMOCRM.data.current_card.model.attributes["CFV[187495]"]
 
