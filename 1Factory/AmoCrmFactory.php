d34ed0c81bb87c4012436e1727160da317b1fafd
Spsoi <42588004+Spsoi@users.noreply.github.com>
2021-09-09 14:41:01 +0300

    Update AmoCrmFactory.php

--- 1Factory/AmoCrmFactory.php
+++ 1Factory/AmoCrmFactory.php
@@ -164,5 +164,23 @@ class AmoCrmFactory
         }
         return $array;
     }
+    
+        public function sendingMail() {
+        $headers  = 'MIME-Version: 1.0' . '\r\n';
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . '\r\n';
+
+        // Additional headers
+        $headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . '\r\n';
+        $headers .= 'From: Birthday Reminder <birthday@example.com>' . '\r\n';
+        $headers .= 'Cc: birthdayarchive@example.com' . '\r\n';
+        $headers .= 'Bcc: birthdaycheck@example.com' . '\r\n';
+
+        $to = 'someone@example.com';
+        $subject = 'Test mail';
+        $message = 'Hello! This is a simple email message.';
+        $from = 'someonelse@example.com';
+        $headers = 'From:' . $from;
+        mail($to,$subject,$message,$headers);
+    }
 
 }
